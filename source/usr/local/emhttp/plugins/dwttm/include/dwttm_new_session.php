08130d52e84c2a3820cc5f8f50ed58effd61bfd1
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-12-17 11:00:58 +0100

    changes

--- source/usr/local/emhttp/plugins/dwttm/include/dwttm_new_session.php
+++ source/usr/local/emhttp/plugins/dwttm/include/dwttm_new_session.php
@@ -1,27 +1,37 @@
 <?php
-header('Content-Type: application/json');
+/* Copyright Derek Macias (parts of code from NUT package)
+ * Copyright macester (parts of code from NUT package)
+ * Copyright gfjardim (parts of code from NUT package)
+ * Copyright SimonF (parts of code from NUT package)
+ * Copyright Dan Landon (parts of code from Web GUI)
+ * Copyright Bergware International (parts of code from Web GUI)
+ * Copyright Lime Technology (any and all other parts of Unraid)
+ *
+ * Copyright desertwitch (as author and maintainer of this file)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ */
+$command = "tmux new-session -d -x 80 -y 24 -P -F '#{session_id}' 'env TERM=xterm /bin/bash'";
 
-// Path to the custom tmux config file
-$tmuxConfig = '/etc/ttmd.conf';
-
-// TMUX command with the custom configuration file
-$command = "tmux -f {$tmuxConfig} new-session -d -P -F \"#{session_id}\"";
-
-// Execute the command and capture the output
 $output = null;
 $returnVar = null;
 exec($command, $output, $returnVar);
 
-// Check if the command was successful and return JSON response
 if ($returnVar === 0 && !empty($output)) {
     echo json_encode([
         'success' => true,
-        'session_id' => $output[0]
+        'session_id' => trim($output[0]);
     ]);
 } else {
     echo json_encode([
         'success' => false,
-        'message' => 'Failed to create a new tmux session.'
+        'message' => 'Failed to create new session.'
     ]);
 }
 ?>
