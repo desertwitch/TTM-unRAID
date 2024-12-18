aaf6b2d3a59a6aaef6d356b2fb904c7816d7547f
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-12-18 19:15:30 +0100

    Update tterminal.php

--- source/usr/local/emhttp/plugins/dwttm/tterminal.php
+++ source/usr/local/emhttp/plugins/dwttm/tterminal.php
@@ -17,15 +17,6 @@
  * included in all copies or substantial portions of the Software.
  *
  */
-require_once '/usr/local/emhttp/plugins/dwttm/include/dwttm_config.php';
-
-if($dwttm_running && !$dwttm_tmux_functional) {
-    @shell_exec("/etc/rc.d/rc.ttmd stop &>/dev/null");
-    echo("<b>Error: Tmux not found or not functional.</b><br>");
-    echo("<b>Error: Please refer to the 'TTM Settings' for more information on this issue.</b>");
-    die();
-}
-
 if (!function_exists('autov')) {
     function autov($file, $ret = false) {
         $docroot ??= ($_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp');
