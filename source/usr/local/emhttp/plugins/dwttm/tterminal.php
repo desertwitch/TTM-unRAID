14bfa78760a8110854e3830dc77dfa6c89434bb8
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-12-19 11:27:37 +0100

    Update tterminal.php

--- source/usr/local/emhttp/plugins/dwttm/tterminal.php
+++ source/usr/local/emhttp/plugins/dwttm/tterminal.php
@@ -130,7 +130,7 @@ $currentSession = isset($_GET['session']) ? $_GET['session'] : null;
         <?php if (!$currentSession): ?>
             <div class="new-session-container" id="new-session-container" onclick="createNewSession()">
                 <div class="plus-icon">+</div>
-                <div class="new-session-text">New Session</div>
+                <div class="new-session-text">Select Session / New Session</div>
             </div>
         <?php else: ?>
             <div id="terminal-container"></div>
