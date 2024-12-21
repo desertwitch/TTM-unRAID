d6312a5b3adf99e1a7abee81d1684ad26f3ac098
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-12-21 16:09:12 +0100

    changes

---
+++ source/usr/local/emhttp/plugins/dwttm/include/dwttm_rename_session.php
@@ -0,0 +1,57 @@
+<?php
+/* Copyright Derek Macias (parts of code from NUT package)
+ * Copyright macester (parts of code from NUT package)
+ * Copyright gfjardim (parts of code from NUT package)
+ * Copyright SimonF (parts of code from NUT package)
+ * Copyright Dan Landon (parts of code from Web GUI)
+ * Copyright Bergware International (parts of code from Web GUI)
+ * Copyright Lime Technology (any and all other parts of Unraid)
+ *
+ * Copyright desertwitch (as author and maintainer of this file)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ */
+header('Content-Type: application/json');
+
+if (!isset($_GET['session']) || !preg_match('/^[a-zA-Z0-9_\-\$]+$/', $_GET['session'])) {
+    echo json_encode([
+        'success' => false,
+        'error' => 'Invalid or missing session ID.'
+    ]);
+    exit;
+}
+
+if (!isset($_GET['session']) || !preg_match('/^[a-zA-Z0-9_\-\$]+$/', $_GET['sessionName'])) {
+    echo json_encode([
+        'success' => false,
+        'error' => 'Invalid or missing session name.'
+    ]);
+    exit;
+}
+
+$sessionID = escapeshellarg($_GET['session']);
+$sessionName = escapeshellarg($_GET['sessionName']);
+
+$command = "tmux rename-session -t {$sessionID} {$sessionName} 2>&1";
+$output = [];
+$returnCode = null;
+exec($command, $output, $returnCode);
+
+if ($returnCode === 0) {
+    echo json_encode([
+        'success' => true
+    ]);
+} else {
+    echo json_encode([
+        'success' => false,
+        'error' => implode("\n", $output)
+    ]);
+}
+exit;
+?>
