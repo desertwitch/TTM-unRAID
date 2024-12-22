18374c94ef549bb88a7f1264d5faa29055f57312
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-12-22 11:10:54 +0100

    changes

--- source/usr/local/emhttp/plugins/dwttm/tterminal.php
+++ source/usr/local/emhttp/plugins/dwttm/tterminal.php
@@ -403,7 +403,7 @@ $currentSession = isset($_GET['session']) ? $_GET['session'] : null;
 
             ws.onclose = () => {
                 term.write('\r\n*** Disconnected from session ***\r\n');
-                term.write('\r\nYou can open a new session by clicking on the + button in the GUI.\r\n');
+                term.write('You can open a new session by clicking on the + button in the GUI.\r\n');
                 freeSession();
             };
 
